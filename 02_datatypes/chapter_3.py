2c04da5925de6181665d34504ccdcdf82b830fc0
aditya <adityayadav135952@gmail.com>
2025-11-09 22:20:32 +0530

    decimal number in python

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,34 @@
+# Integer
+
+black_tea_grams = 14
+ginger_grams = 3 
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea is {total_grams}")
+
+remaing_tea = black_tea_grams - ginger_grams
+print(f"Total grams of remaining tea is {remaing_tea}")
+
+milk_litres = 7 
+serving = 4 
+milk_per_serving = milk_litres / serving
+print(f"Milk per serving is {milk_per_serving}")
+
+books = 7 
+reader = 4
+books_per_reader = books // reader       #Here we use '//' which remove the decimals values. 
+print(f"Total books per reader is {books_per_reader}")
+
+# For remainder 
+a = 10; 
+b = 3; 
+remainder = a % b; 
+
+print(remainder)
+
+# Exponential Calculation here we use '**'
+
+a = 2; 
+b = 3; 
+exponent = a ** 3; 
+print (exponent)
