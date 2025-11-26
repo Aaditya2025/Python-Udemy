bcbe574025a6b7a65e8e5b8c787cc3482e93b173
aditya <adityayadav135952@gmail.com>
2025-11-26 21:58:01 +0530

    order name assigning problem

---
+++ 04_loops/03_problems.py
@@ -0,0 +1,13 @@
+"""
+You receive a list of names for chai orders. 
+The goal is to print out the order queue.
+Task: 
+1. Use a list of names. 
+2. Print: "Order ready for [name]"
+
+"""
+
+orders = ["aditya", "aman", "becky", "marcos", "bruno"]
+
+for name in orders: 
+    print(f"Order ready for {name}")
