4e713bdbaac0896ee2e668a602b68b1e78a53bcb
aditya <adityayadav135952@gmail.com>
2025-11-27 23:17:18 +0530

    zip in python

--- 04_loops/05_order_summary.py
+++ 04_loops/05_order_summary.py
@@ -1,1 +1,14 @@
-names = ["Hitesh", "Meera", "Sam", "Ali"]
+""" 
+You're preparing an order summary with customer names and their total bill. 
+Task: 
+1. Use two lists: one for names and one for bills. 
+2. Print: "[Name] paid ₹[amount]"
+"""
+
+
+names = ["Aditya", "Meera", "Sam", "Ali"]
+bills = [50, 70, 100, 55]
+
+for names, amounts in zip(names, bills):
+    print(f"{names} paid {amounts} rupees")
+
