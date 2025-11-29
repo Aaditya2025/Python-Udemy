630feb5fd16c6b70ba647610733c0b5baae28c12
aditya <adityayadav135952@gmail.com>
2025-11-29 22:29:55 +0530

    functions task

---
+++ 05_functions/01_duplicatoin.py
@@ -0,0 +1,7 @@
+def print_Order(name, chai_order):
+    print(f"{name} ordered {chai_order} chai!")
+
+
+print_Order("Aditya", "Masala"); 
+print_Order("Dinesh", "Milk"); 
+print_Order("vipin", "Ginger"); 
