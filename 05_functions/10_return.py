54d96baa8a632d4ddfc5e41f5e107f6a525cf206
aditya <adityayadav135952@gmail.com>
2025-12-13 23:59:55 +0530

    return type

---
+++ 05_functions/10_return.py
@@ -0,0 +1,8 @@
+# There are three things functions may return: 
+# 1. Nothing: imlicitly return None.  2. One Value, 3. Mulitple Value 
+# 4. Early from function
+
+def make_chai(): 
+    return "Here is your masala chai" 
+
+print(make_chai())
