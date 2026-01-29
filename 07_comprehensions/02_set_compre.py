b72b279c85520a67a8c4b1a5a0f58db39c755a7a
aditya <adityayadav135952@gmail.com>
2026-01-29 09:24:46 +0530

    set comprehension

--- 07_comprehensions/02_set_compre.py
+++ 07_comprehensions/02_set_compre.py
@@ -5,3 +5,13 @@ favourite_chais = [
 
 unique_chai = {chai for chai in favourite_chais }
 print(unique_chai)
+
+recipes = {
+    "Masala Chai": ["ginger", "cardamom", "clove"], 
+    "Elaichi Chai": ["cardamom", "milk"], 
+    "Spicy Chai": ["ginger", "black pepper", "clove"]
+}
+
+unique_spices = {spice for ingredients in recipes.values() for spice in ingredients}
+
+print(unique_spices)
