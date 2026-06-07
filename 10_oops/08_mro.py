3907783920e8b4dd57c5717c7f60235fbc5b48c7
aditya <adityayadav135952@gmail.com>
2026-06-07 13:06:54 +0530

    mro in python

---
+++ 10_oops/08_mro.py
@@ -0,0 +1,17 @@
+# Method Resolution Order : 
+
+class A: 
+    label = "A: Base Class"
+
+class B(A): 
+    label = "B: Masala blend" 
+
+class C(A): 
+    label = "C: Herbal blend" 
+
+class D(B, C):    # B is the first argument so this class consider B as the first parent and print that value. 
+    pass
+
+cup = D()
+
+print(cup.label)
